688e4cee57cdd0d7d887e3c5a87b4bbd81119842
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-11-23 00:23:18 +0100

    Displaying representations for a single pubkey

--- src/view/pubkey.rs
+++ src/view/pubkey.rs
@@ -14,6 +14,9 @@
 use gtk::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::str::FromStr;
+
+use lnpbp::{bitcoin, secp256k1};
 
 static UI: &'static str = include_str!("../../ui/pubkey.glade");
 
@@ -28,6 +31,9 @@ pub enum PkType {
 pub struct PubkeyDlg {
     //#[glade_id="assetIssue"]
     dialog: gtk::Dialog,
+    msg_box: gtk::Box,
+    msg_label: gtk::Label,
+    msg_image: gtk::Image,
 
     name_field: gtk::Entry,
     pubkey_field: gtk::Entry,
@@ -39,12 +45,31 @@ pub struct PubkeyDlg {
     bip44_radio: gtk::RadioButton,
     custom_radio: gtk::RadioButton,
 
-    pk_cache: String,
+    network_combo: gtk::ComboBox,
+
+    uncompressed_display: gtk::Entry,
+    compressed_display: gtk::Entry,
+    xcoordonly_display: gtk::Entry,
+
+    pkh_display: gtk::Entry,
+    wpkh_display: gtk::Entry,
+    wpkh_sh_display: gtk::Entry,
+    taproot_display: gtk::Entry,
 }
 
 impl glade::View for PubkeyDlg {
     fn load_glade() -> Result<Rc<RefCell<Self>>, glade::Error> {
         let builder = gtk::Builder::from_string(UI);
+
+        let msg_box = builder
+            .get_object("messageBox")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let msg_image = builder
+            .get_object("messageImage")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let msg_label = builder
+            .get_object("messageLabel")
+            .ok_or(glade::Error::WidgetNotFound)?;
 
         let name_field = builder
             .get_object("nameField")
@@ -67,8 +92,39 @@ impl glade::View for PubkeyDlg {
         let custom_radio = builder
             .get_object("deriveCustom")
             .ok_or(glade::Error::WidgetNotFound)?;
+
+        let network_combo = builder
+            .get_object("blockchainCombo")
+            .ok_or(glade::Error::WidgetNotFound)?;
+
+        let uncompressed_display = builder
+            .get_object("uncompressedDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let compressed_display = builder
+            .get_object("compressedDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let xcoordonly_display = builder
+            .get_object("xonlyDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+
+        let pkh_display = builder
+            .get_object("pkhDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let wpkh_display = builder
+            .get_object("wpkhDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let wpkh_sh_display = builder
+            .get_object("wpkhShDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+        let taproot_display = builder
+            .get_object("taprootDisplay")
+            .ok_or(glade::Error::WidgetNotFound)?;
+
         let me = Rc::new(RefCell::new(Self {
             dialog: glade_load!(builder, "pubkeyDlg")?,
+            msg_box,
+            msg_image,
+            msg_label,
             name_field,
             pubkey_field,
             xpub_field,
@@ -76,7 +132,14 @@ impl glade::View for PubkeyDlg {
             hd_radio,
             bip44_radio,
             custom_radio,
-            pk_cache: none!(),
+            network_combo,
+            uncompressed_display,
+            compressed_display,
+            xcoordonly_display,
+            pkh_display,
+            wpkh_display,
+            wpkh_sh_display,
+            taproot_display,
         }));
 
         me.as_ref().borrow().pubkey_field.connect_changed(
@@ -107,6 +170,13 @@ impl glade::View for PubkeyDlg {
             }),
         );
 
+        me.as_ref().borrow().network_combo.connect_changed(
+            clone!(@weak me => move |_| {
+                let me = me.borrow();
+                me.update_ui()
+            }),
+        );
+
         Ok(me)
     }
 }
@@ -132,5 +202,69 @@ impl PubkeyDlg {
             self.sk_radio.set_active(pk_type == PkType::Single);
             self.hd_radio.set_active(pk_type == PkType::Hd);
         }
+        self.update_ui();
+    }
+
+    pub fn update_ui(&self) {
+        match self.update_ui_internal() {
+            Ok(_) => {
+                self.msg_box.set_visible(false);
+            }
+            Err(err) => {
+                self.msg_label.set_text(&err);
+                self.msg_box.set_visible(true);
+            }
+        }
+    }
+
+    pub fn update_ui_internal(&self) -> Result<(), String> {
+        let network = match self.network_combo.get_active() {
+            Some(0) => bitcoin::Network::Bitcoin,
+            Some(1) => bitcoin::Network::Testnet,
+            Some(2) => bitcoin::Network::Testnet,
+            None => Err("You need to specify blockchain type")?,
+            _ => Err("Unsupported blockchain")?,
+        };
+
+        let pk_type = if self.sk_radio.get_active() {
+            let pk_str = self.pubkey_field.get_text();
+            let pk = secp256k1::PublicKey::from_str(&pk_str)
+                .map_err(|err| err.to_string())?;
+
+            self.uncompressed_display.set_text(
+                &bitcoin::PublicKey {
+                    compressed: false,
+                    key: pk,
+                }
+                .to_string(),
+            );
+
+            let pk = bitcoin::PublicKey {
+                compressed: true,
+                key: pk,
+            };
+            self.compressed_display.set_text(&pk.to_string());
+            self.xcoordonly_display.set_text("Not yet supported");
+
+            self.pkh_display
+                .set_text(&bitcoin::Address::p2pkh(&pk, network).to_string());
+            self.wpkh_display.set_text(
+                &bitcoin::Address::p2wpkh(&pk, network)
+                    .expect("The key is compressed")
+                    .to_string(),
+            );
+            self.wpkh_sh_display.set_text(
+                &bitcoin::Address::p2shwpkh(&pk, network)
+                    .expect("The key is compressed")
+                    .to_string(),
+            );
+            self.taproot_display.set_text("Not yet supported");
+
+            PkType::Single
+        } else {
+            PkType::Hd
+        };
+
+        Ok(())
     }
 }
